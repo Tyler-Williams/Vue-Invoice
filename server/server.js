e299bbc324269eae09d1de346f50e2c088cabeb1
Tyler-Williams <github@tylerclarkwilliams.com>
2018-05-01 16:22:47 -0700

    Completed Api requests and debugging

--- server/server.js
+++ server/server.js
@@ -103,19 +103,28 @@ app.post('/invoice', multipartMiddleware, (req, res) => {
     )`
 
     db.serialize(() => {
-      db.run(sql, (err) => {
+      db.run(sql, function(err) {
         if (err) {
+          console.log('Error inserting invoice into invoices table')
           throw err
         }
+
         let invoice_id = this.lastID
-        for (let i = 0; i< req.body.txn_names.length; i++) {
+
+        for (let i = 0; i < req.body.txn_names.length; i++) {
           let query = `INSERT INTO transactions(name,price,invoice_id)
             VALUES(
-              '${req.body.txn_names[i]}'
-              '${req.body.txn_prices[i]}'
+              '${req.body.txn_names[i]}',
+              '${req.body.txn_prices[i]}',
               '${invoice_id}'
             )`
-          db.run(query)
+//          console.log(`Query: ${query}`)
+          db.run(query, (err) => {
+            if (err) {
+              console.log('Error inserting transaction into transactions table')
+          throw err
+            }
+          })
         }
         return res.json({
           status: true,
@@ -145,7 +154,7 @@ app.get('/invoice/user/:user_id', multipartMiddleware, (req, res) => {
 app.get('/invoice/user/:user_id/:invoice_id', multipartMiddleware, (req, res) => {
   let db = new sqlite3.Database(databaseLocation)
   let sql = `SELECT * FROM invoices LEFT JOIN transactions ON invoices.id=transactions.invoice_id
-    WHERE user_id='${req.param.user_id}' AND invoice_id='${req.params.invoice_id}'`
+    WHERE user_id='${req.params.user_id}' AND invoice_id='${req.params.invoice_id}'`
 
   db.all(sql, [], (err, rows) => {
     if (err) {
